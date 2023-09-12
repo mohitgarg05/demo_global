df1273802a048a3155b6ec8f0578047c0af39cd8
Mohit garg <141032758+Mohitrohido@users.noreply.github.com>
2023-09-12 10:42:39 +0530

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import Home from "./Pages/Home";
 import Certificates2 from "./Components/Certificates2";
 import ContactUs from "./Pages/ContactUs";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 
 function App() {
   
   
   return (
     <BrowserRouter>
-            <Switch>
-                <Route exact path = '/' component = {Home} />
-                <Route exact path = '/certificates' component = {Certificates2} />
-                <Route exact path = '/contact-form' component = {ContactUs} />
-    
-            </Switch>
+            <Routes>
+                <Route exact path = '/' element={<Home />} />
+                <Route exact path = '/certificates' element={<Certificates2 />} />
+                <Route exact path = '/contact-form' element={<ContactUs />} />
+            </Routes>
         </BrowserRouter>
   )
 }
